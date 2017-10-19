50e85d9af8ab3ec7bedd91104567a5276dcbabc9
Martin Poirier Theoret <mpoiriert@gmail.com>
2017-10-19 15:49:56 -0400

    support for @package as class

--- ServiceContainer.js
+++ ServiceContainer.js
@@ -5,7 +5,6 @@ module.exports = (function () {
     const Builder = require('./lib/Builder');
     // Create an instance of the builder with the proper dependencies injected
     return new Builder(
-        require('fs'),
         require,
         require('./lib/Container'),
         require('./lib/Definition')
